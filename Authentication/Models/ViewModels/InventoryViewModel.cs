d52e93f35044fd67e861994e382c4a44f2bd9ee1
ThanawatUnp <thanawat.unp@gmail.com>
2024-05-15 13:31:48 +0700

    add inventory #2

--- Authentication/Models/ViewModels/InventoryViewModel.cs
+++ Authentication/Models/ViewModels/InventoryViewModel.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Authentication.ViewModels
+namespace Authentication.Models
 {
-    public class Inventories
+    public class InventoryViewModel
     {
         public Guid id { get; set; }
         public string itemCode { get; set; }
         public string itemName { get; set; }
+        public Guid itemCategoryId { get; set; }
         public string itemCategory { get; set; }
         public double? cost { get; set; }
         public int? unit { get; set; }
